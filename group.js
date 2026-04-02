c1b6454b4e79d959c8543db4678c95250f69a57e
Talib460 <meripubg455@gmail.com>
2026-04-03 03:09:22 +0500

    Delete group.js

--- group.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "kick",
-  execute: async (sock, m) => {
-    let user = m.message.extendedTextMessage.contextInfo.participant;
-    await sock.groupParticipantsUpdate(m.key.remoteJid, [user], "remove");
-  }
-};
